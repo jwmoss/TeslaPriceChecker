b99b7056cdf16c80bac099f1a869a08a4a871e75
jwmoss <jwmoss88@gmail.com>
2020-11-24 18:56:59 -0500

    changes

--- teslapricechecker.psm1
+++ teslapricechecker.psm1
@@ -6,125 +6,52 @@ function Get-LoanAmount {
     )   
     
     process {
-        [int32]([Microsoft.VisualBasic.Financial]::Pmt(0.002075, 84, - $price, 0, 0))
-    }
+        $price2 = $price - 2000
+        $convertedprice = [int32]([Microsoft.VisualBasic.Financial]::Pmt(0.001999, 84, - $price2, 0, 0))
     
+        [PSCustomObject]@{
+            MonthlyWithDeposit = $convertedprice * 1.03
+            MonthlyWithInsurance = ($convertedprice * 1.03)  + 50
+        }
+    }
 }
 
-function Get-OnlyUsedTesla {
+function Get-TeslaPrice {
     [CmdletBinding()]
-    param (
-
-        [Parameter(Mandatory)]  
-        [ValidateSet("3", "S", "ALL")]
+    param ( 
         [string]
-        $Model,
-
-        [ValidateSet("AWD", "RWD", "ALL")]
-        [string]
-        $Type,
-
-        [string]
-        $PriceStart = "33,000",
-
-        [string]
-        $PriceEnd = "41,000"
+        $URL
     )
     
-    $url = "https://onlyusedtesla.com/listings/?"
-
-    $Body = @{
-        "_sft_listing_type"     = "for-sale"
-        "_sfm_out_asking_price" = "$pricestart+$priceend"
-    }
-
-    switch ($Model) {
-        "S" {  
-            $body.Add("_sft_model","model-s")
-        }
-        "3" {
-            switch ($Type) {
-                "AWD" { 
-                    $body.Add("_sft_model","model-3")
-                    $body.Add("_sft_battery","long-range-awd")
-                }
-                "RWD" { 
-                    $body.Add("_sft_model","model-3")
-                    $body.Add("_sft_battery","long-range-rwd")
-                }
-                "ALL" {
-                    $body.Add("_sft_model","model-3")
-                }
-                Default {
-                    $body.Add("_sft_model","model-3")
-                }
-            }
-        }
-        "ALL" {
-            $Body = @{
-                "_sft_listing_type"     = "for-sale"
-                "_sfm_out_asking_price" = "$pricestart+$priceend"
-            }
-        }
-    }
-
-    $Locations = @("Virginia|North Carolina|South Carolina|Georgia|Florida")
+    $driver = Start-SeFirefox -StartURL $Url -Headless
+    $price = (Find-SeElement -By XPath -Selection "/html/body/div[1]/div/div[2]/div/div/div[2]/div[1]/p[3]" -Driver $driver).Text.Split(" ")
     
-    try {
-        $page = Invoke-WebRequest -uri $url -body $body
-    }
-    catch {
-        Write-Warning "Could not download from $Url" 
-        break
-    }
-
-    $rawhtml = ConvertFrom-Html $page.RawContent
+    $location = (Find-SeElement -By XPath -Selection "/html/body/div[1]/div/div[2]/div/div/div[2]/div[2]/p[2]/span" -Driver $driver).Text.Split(" ")
     
-    $table = ((($rawhtml.Descendants()).where{ $_.HasClass("search-filter-results") -eq "True" }))
+    $originalPrice = ((($Price[1] -replace "HISTORY:").Trim()) -replace "\$")
+    $price = $originalPrice - 2000
+    $monthly = [int32]([Microsoft.VisualBasic.Financial]::Pmt(0.002075, 84, - $price, 0, 0))
     
-    $cars = $table.Descendants().where{ $_.HasClass("out-list-item") -eq "True" }
+    $city = ($location[1..2] -replace "Full")[0]
+    $state = ($location[1..2] -replace "Full")[1]
     
-    $cars | ForEach-Object {
-        $carpage = $null
-        $car = $_
-        $linktoCarPage = Invoke-WebRequest $car.SelectSingleNode(".//a").Attributes["href"].Value
-        $link = $car.SelectSingleNode(".//a").Attributes["href"].Value
-        $carpage = ConvertFrom-Html $linktoCarPage.rawContent
-        $location = $car.Descendants().Where( { $_.InnerText -match "location" -and $_.Name -eq "li" }).Innertext -replace "Location"
-        $state = ($location -split ",")[1]
-        $Year = $carpage.Descendants().Where( { $_.InnerText -match "Year" -and $_.Name -eq "li" }).Innertext -replace "Year"
-        $battery = $carpage.Descendants().Where( { $_.InnerText -match "Battery" -and $_.Name -eq "li" }).Innertext -replace "Battery"
-        $price = "{0:C2}" -f (([int](($car.Descendants().where{ $_.HasClass("asking-price") -eq "True" }).InnerText -replace '\D+(\d+)', '$1'))/100)
-        $pricecalc = (([int32](($car.Descendants().where{ $_.HasClass("asking-price") -eq "True" }).InnerText -replace '\D+(\d+)', '$1'))/100)
-        switch -Regex ($Battery) {
-            "RWD" {  
-                $zeroto60 = "5.0"
-            }
-            "AWD" {
-                $zeroto60 = "4.4"
-            }
-            Default {
-                $zeroto60 = $null
-            }
-        }
-
-        if ($state -match $Locations) {
-            [PSCustomObject]@{
-                AskingPrice = $price
-                Monthly     = Get-LoanAmount -Price $pricecalc
-                Miles       = [int32](($car.Descendants().Where( { $_.InnerText -match "mileage" -and $_.Name -eq "li" })).InnerText -replace "\D")
-                Battery     = $battery
-                Location    = $location
-                ZeroTo60    = $zeroto60
-                Date        = Get-Date ($car.Descendants().Where( { $_.InnerText -match "Listing Date" -and $_.Name -eq "li" }).Innertext -replace "Listing Date").Trim()
-                Year        = $year
-                Link        = $link
-                #VIN         = $carpage.Descendants().Where( { $_.InnerText -match "VIN" -and $_.Name -eq "li" }).Innertext -replace "VIN #"
-                Color       = $carpage.Descendants().Where( { $_.InnerText -match "Color" -and $_.Name -eq "li" }).Innertext -replace "Color"
-                AutoPilot   = ($carpage.Descendants().Where( { $_.InnerText -match "AutoPilot" -and $_.Name -eq "li" }).Innertext -replace "AutoPilot").Trim()
-            }   
-        }
+    if ($state -like "*,*") {
+        [string]$city = ($location[1..2] -replace "Full")[0..1] -replace ","
+        $state = (($location[3]) -replace "Full").Trim()
     }
+    else {
+        $city = $city -replace ","
+    }
+    
+    [PSCustomObject]@{
+        City                 = $city
+        State                = $state
+        OriginalPrice        = $originalPrice 
+        Price                = $price
+        Monthly              = $monthly
+        MonthlyWithInsurance = $monthly + "50"
+    }
+    Stop-SeDriver -Target $driver
 }
 
 Export-ModuleMember -Function *
